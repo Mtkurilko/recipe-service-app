26948d72bda3c2d4ff2bcd24621494038a26aeb2
mtk2024 <drmichael1519@gmail.com>
2024-11-06 20:24:05 -0500

    Added Favicon, Title, & Logo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-import RecipeSearch from "./components/recipeSearch";
-import MealPlanner from "./components/mealPlanner";
-import ShoppingList from "./components/shoppingList";
 import Home from "./pages/home";
 import {
     BrowserRouter as Router,
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,14 @@ import RecipeSearch from "../components/recipeSearch";
 import "./home.css"
 import NavBurger from "../components/navBurger";
 import RecipeDisplay from "../components/recipeDisplay";
+import logo from "../images/RecipeGuru.png";
 
 function Home () {
     return (
         <div className="pBody">
             <div className="homeHeader">
                 <NavBurger />
+                <a href="/"><img id="appLogo" src={logo} alt="Recipe Guru" /></a>
                 <RecipeSearch />
             </div>
             <RecipeDisplay />
