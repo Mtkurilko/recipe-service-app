5534f2bc6a914dca33428d918576348574edd1c4
mtk2024 <drmichael1519@gmail.com>
2024-11-06 19:59:14 -0500

    Added Recipes from prop

--- src/components/recipeDisplay.js
+++ src/components/recipeDisplay.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import './recipeDisplay.css'
-import threeDots from '../images/3dots.png'
+import './recipeDisplay.css';
+import threeDots from '../images/3dots.png';
 
-function RecipeDisplay(recipes = []) {
+const defaultRecipes = {
+    1: {
+        title: 'Recipe 1 Title',
+        desc: 'This is a description for Recipe 1...',
+        attributes: ['Dairy-Free', 'Low-Carb'],
+        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
+    },
+    2: {
+        title: 'Recipe 2 Title',
+        desc: 'This is a description for Recipe 2...',
+        attributes: ['Vegan', 'Gluten-Free'],
+        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
+    },
+    3: {
+        title: 'Recipe 3 Title',
+        desc: 'This is a description for Recipe 3...',
+        attributes: ['Vegan', 'Gluten-Free'],
+        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
+    },
+    4: {
+        title: 'Recipe 4 Title',
+        desc: 'This is a description for Recipe 4...',
+        attributes: ['Vegan', 'Gluten-Free'],
+        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
+    },
+    5: {
+        title: 'Recipe 5 Title',
+        desc: 'This is a description for Recipe 5...',
+        attributes: ['Vegan', 'Gluten-Free'],
+        ingredients: ['Ingredient 1', 'Ingredient 2', 'Ingredient 3'],
+    }
+};
+
+function RecipeDisplay({ recipes = defaultRecipes }) {
+    const changeActionMenu = (index) => {
+        const menu = document.getElementById(`actionMenu-${index}`);
+        if (menu.style.display !== 'block') {
+            menu.style.display = 'block';
+        } else {
+            menu.style.display = 'none';
+        }
+    };
+
     return (
         <div className="recipesContainer">
-            <div className="recipe">
-                <h3 className="recipeTitle">Example Title</h3>
-                <button className="actionButton" ><img src={threeDots} alt="Action Button" id="actionIMG" /></button>
-                <div className="recipeAttribute">Nuts</div>
-                <br />
-                <ul className="ingredientList">
-                    <li className="ingredient">Test Ingredient</li>
-                    <li className="ingredient">Test Ingredient</li>
-                    <li className="ingredient">Test Ingredient</li>
-                    <li className="ingredient">Test Ingredient</li>
-                    <li className="ingredient">Test Ingredient</li>
-                </ul>
-                <p className="recipeDescription">
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                </p>
-            </div>
+            {Object.entries(recipes).map(([recipeID, recipe], index) => (
+                <div className="recipe" key={recipeID}>
+                    <h3 className="recipeTitle">{recipe.title}</h3>
+                    <button className="actionButton" onClick={() => changeActionMenu(index)}>
+                        <img src={threeDots} alt="Action Button" id="actionIMG" />
+                    </button>
+                    <div id={`actionMenu-${index}`} className="actionMenu">
+                        <button className="actionMenuButton">+ Shopping List</button>
+                        <br />
+                        <button className="actionMenuButton">+ Meal Plan</button>
+                    </div>
+                    <div className="attributes">
+                        {recipe.attributes.map((attribute) => (
+                            <div className="recipeAttribute">{attribute}</div>
+                        ))}
+                    </div>
+                    <br />
+                    <div className="desc&List">
+                        <ul className="ingredientList">
+                            {recipe.ingredients.map((ingredient, i) => (
+                                <li className="ingredient" key={i}>{ingredient}</li>
+                            ))}
+                        </ul>
+                        <div className="recipeDescription">
+                            {recipe.desc}
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
